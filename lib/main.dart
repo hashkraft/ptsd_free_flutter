02fd7df9a294bfb17d20bfd76a35ed1b6b7b3c35
Shubham Kunal <shubhamkunal@Shubhams-MacBook-Air.local>
2023-11-09 22:13:44 +0530

    static images

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ptsd_free/router/router.dart';
 import 'package:ptsd_free/notifications/notifications_service.dart';
+import 'package:ptsd_free/widgets/custom_colored_text.dart';
 
 Future<void> main() async {
   await initializeNotification();
@@ -62,30 +63,122 @@ class _StartInformationState extends State<StartInformation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          (showinfo2)
+              ? " 3 Keys to Live PTSD Free"
+              : "Stress Stopper Breathwork",
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
       body: Container(
         child: (showinfo2 == false)
             ? Column(
                 children: [
-                  const SizedBox(height: 150),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              showinfo2 = true;
-                            });
-                          },
-                          child: const Text("I'm Okay"),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text("Help me"),
-                        ),
-                      ],
+                  const SizedBox(height: 16),
+                  CustomColoredText(
+                    text: "Take Action During Your Reaction",
+                    hexColor: "#F73C00",
+                    size: 18,
+                    weight: 500,
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
+                            "assets/images/img_comfortable_eyes.png",
+                            width: 250,
+                          ),
+                          const SizedBox(height: 24),
+                          Image.asset(
+                            "assets/images/img_breathe_deep_2.png",
+                            width: 250,
+                          ),
+                          const SizedBox(height: 24),
+                          Image.asset(
+                            "assets/images/img_silent_okay_2.png",
+                            width: 250,
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        "assets/images/breathwork_touch_heart.png",
+                        width: 100,
+                        height: 200,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  CustomColoredText(
+                    text: "REPEAT UNTIL YOUR STRESS IS GONE",
+                    hexColor: "#0057B8",
+                    size: 18,
+                    weight: 500,
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: CustomColoredText(
+                      text:
+                          "Stress Stopper Breathwork quickly activates your body's relaxation response",
+                      hexColor: "#2C3351",
+                      size: 16,
+                      weight: 500,
                     ),
-                  )
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              return Colors.blue;
+                            },
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            showinfo2 = true;
+                          });
+                        },
+                        child: CustomColoredText(
+                          text: "I'm Okay",
+                          hexColor: "#FFFFFF",
+                          size: 16,
+                          weight: 500,
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              return Colors.red;
+                            },
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: CustomColoredText(
+                          text: "Help me",
+                          hexColor: "#FFFFFF",
+                          size: 16,
+                          weight: 500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    "assets/images/img_certified_american_institute.png",
+                    width: 280,
+                    height: 100,
+                  ),
                 ],
               )
             : GestureDetector(
@@ -93,17 +186,55 @@ class _StartInformationState extends State<StartInformation> {
                   // Navigator.of(context).pop();
                   context.go("/home");
                 },
-                child: SizedBox(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
                   height: MediaQuery.of(context).size.height,
                   width: MediaQuery.of(context).size.width,
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Text 1"),
-                      Text("Text 2"),
-                      Text("Text 3"),
-                      Text("Text 4"),
-                      Text("Text 5"),
+                      const SizedBox(height: 32),
+                      CustomColoredText(
+                        text: "1. Stopper Tab",
+                        hexColor: "#F73C00",
+                        size: 18,
+                        weight: 500,
+                      ),
+                      CustomColoredText(
+                        text:
+                            "Will help you learn how to stop your stress reactions. Use it every time you become stressed.",
+                        hexColor: "#2C3351",
+                        size: 18,
+                        weight: 500,
+                      ),
+                      const SizedBox(height: 16),
+                      CustomColoredText(
+                        text: "2. Resolve Tab",
+                        hexColor: "#00B73A",
+                        size: 18,
+                        weight: 500,
+                      ),
+                      CustomColoredText(
+                        text:
+                            "Will help you heal the traumatic memories fueling your stress reactions. Use it weekly.",
+                        hexColor: "#2C3351",
+                        size: 18,
+                        weight: 500,
+                      ),
+                      const SizedBox(height: 16),
+                      CustomColoredText(
+                        text: "3. My Meds",
+                        hexColor: "#0057B8",
+                        size: 18,
+                        weight: 500,
+                      ),
+                      CustomColoredText(
+                        text:
+                            "Will help you structure a meditation practice. Meditate atleast once a day for 20 minutes.",
+                        hexColor: "#2C3351",
+                        size: 18,
+                        weight: 500,
+                      ),
                     ],
                   ),
                 ),
